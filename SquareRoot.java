2fcf5d3223a352dcff719db6053e8bb4d06e080a
TymonaTata <36015858+TymonaTata@users.noreply.github.com>
2018-06-30 00:14:50 +0200

    Update SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -4,32 +4,34 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class SquareRoot {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         Scanner sc = new Scanner(System.in);
-        double number;
-        double result;
+        double number =0;
+        double result = 0;
         boolean wrongInput = true;
 
         while(wrongInput) {
             try {
-                System.out.println("Podaj dowolną liczbę dodatnią:");
+                System.out.println("Podaj dowolną dodatnią liczbę:");
                 number = sc.nextDouble();
-
-                if (number < 0) throw new IllegalArgumentException();
 
                 wrongInput = false;
                 result = Math.sqrt(number);
-                System.out.println("Pierwiastek kwadratowy z liczby " + number + " wynosi: " + result);
+
 
             } catch (InputMismatchException e) {
                 System.out.println("Podałeś niedozwolony znak. Spróbuj jeszcze raz!");
 
-            } catch (IllegalArgumentException e) {
-                System.out.println("Podałeś liczbę ujemną. Spróbuj jeszcze raz! ");
+
             } finally {
                 sc.nextLine();
             }
         }
         sc.close();
+        if (number < 0 ) {
+            throw new IllegalArgumentException("Liczba powinna być większa lub równa 0");
+        }
+        System.out.println("Pierwiastek kwadratowy z liczby " + number + " wynosi: " + result);
+
     }
 }
